733a4e0224349db4141b1d20eb66c093bdb4a74a
Abdullah Baig <abdullahhussainbaig1997@gmail.com>
2018-10-14 16:54:48 +0530

    add something for authentication

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     // 'url' : 'mongodb://localhost/STORE',
 
-    'url' : 'mongodb://mdabdullahbaig1997:abdul7438@ds131763.mlab.com:31763/dreamstore',
+    'url' : 'mongodb://mdabdullahbaig1997:abdul7438@ds131763.mlab.com:31763/dreamstore/yourDB?authSource=yourDB&w=1',
     useNewUrlParser: true
 };
