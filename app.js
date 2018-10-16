a6b2cfb6e87198f9035afa8ea55f5647370d5c9e
Abdullah Baig <abdullahhussainbaig1997@gmail.com>
2018-10-16 23:56:07 +0530

    Add icon for website

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ var express             =  require("express"),
        indexRoutes       =  require("./routes/index"), 
        commentRoutes     =  require("./routes/comments"),
        categoryRoutes    =  require("./routes/categories");
-
+     //  var deferredPrompt;
+    var navigator = require('web-midi-api');
 
    var configDB = require('./config/database.js');
 
@@ -73,7 +74,94 @@ app.use(commentRoutes);
 app.use(categoryRoutes);
 
 //----------------------------------------------------------------------------------------------------------------
+//service worker
+// if (!window.Promise) {
+//     window.Promise = Promise;
+//   }
 
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+        console.log('Service worker register!');
+
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+};
+// window.addEventListener('beforeinstallprompt', function(event) {
+//     console.log('beforeinstallprompt fired');
+//     event.preventDefault();
+//     defferredPrompt = event;
+//     return false;
+// });
+
+// var promise = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         reject({code: 500, message: "An error occurred!"});
+//        // resolve('This is executed once the timer is done!');
+//     }, 3000);
+// });
+
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://httpbin.org/ip');
+// xhr.responseType = 'json';
+
+// xhr.onload = function() {
+//     console.lod(xhr.response);
+// };
+
+// xhr.onerror = function() {
+//     console.log('Error!');
+// };
+
+// xhr.send();
+
+// fetch('https://httpbin.org/ip')
+//     .then(function(response) {
+//         console.log(response);
+//         response.json();
+//     })
+//         .then(function(data) {
+//             console.log(data);
+//         })
+//         .catch(function(err) {
+//             console.log(err);
+//         });
+
+    //     fetch('https://httpbin.org/post', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json'
+    //         },
+    //         mode: 'cors',
+    //         body: JSON.stringify( {message: 'Does this work?'})
+    //     })
+    // .then(function(response) {
+    //     console.log(response);
+    //     response.json();
+    // })
+    //     .then(function(data) {
+    //         console.log(data);
+    //     })
+    //     .catch(function(err) {
+    //         console.log(err);
+    //     });
+
+// promise.then(function(text) {
+//     return text;
+// }).then(function(newText) {
+//     console.log(newText);
+// }).catch(function(err) {
+//     console.log(err.code,err.message)
+// });
+
+//console.log('This is executed right after setTimeout()');
 app.get("/home/currentuser/profile", function(req,res){
     res.render("profile/currentprofile");
 });
@@ -91,5 +179,5 @@ app.get("/home/currentseller/profile", function(req,res){
 
 
 app.listen(port, function(){
-    console.log("server started!!!")
+    console.log("server started!!!");
 }); 
