c74f6df6493e059b8fcfa4a5d47f4d3223f2a07d
Abdullah Baig <abdullahhussainbaig1997@gmail.com>
2018-10-23 18:17:45 +0530

    add time zone india

--- models/cart.js
+++ models/cart.js
@@ -33,7 +33,7 @@ var cartSchema = new mongoose.Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now() + 34200000
+        default: Date.now()
     },
     
     author:  {
