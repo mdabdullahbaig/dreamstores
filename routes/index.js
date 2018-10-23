369587f08e8a4050ebd3520d8f00784321f2619a
Abdullah Baig <abdullahhussainbaig1997@gmail.com>
2018-10-23 11:29:22 +0530

    add nothing

--- routes/index.js
+++ routes/index.js
@@ -97,124 +97,14 @@ router.get("/logout",function(req,res){
     req.flash("success", "You are successfully Logged Out!!!");
     res.redirect("/home");
 });
-//close
-// //forgot  password
-// router.get ('/forgot', function(req,res) {
-//     res.render('forgot');
-// });
 
-// router.post('/forgot', function(req,res,next) {
-//     async.waterfall( [
-//         function(done) {
-//             crypto.randomBytes(20, function(err, buf) {
-//                 var token = buf.toString('hex');
-//                 done(err, token);
-//             });
-//         },
-//         function(token, done) {
-//             User.findOne({ email: req.body.email}, function(err,user) {
-//                 if(!user) {
-//                     req.flash('error', 'No account with that email address exists.');
-//                     return res.redirect('/forgot');
-//                 }
-//                 user.resetPasswordToken = token;
-//                 user.resetPasswordExpires = Date.now() + 3600000; //1 hour
-//                 user.save(function(err) {
-//                     done(err, token, user);
-//                 });
-//             });
-//         },
-//         function(token, user, done) {
-//             var smtpTransport = nodemailer.createTransport({
-//                 service: 'Gmail',
-//                 auth: {
-//                     user: 'abdullahhussainbaig1997@gmail.com',
-//                     pass: 'Ahussain@2014'
-//                 }
-//             });
-//             var mailOptions = {
-//                 to: user.email,
-//                 from: 'abdullahhussainbaig1997@gmail.com',
-//                 subject: 'Node.js Password Reset',
-//                 text: 'you are receiving this because you (or someone else) have requested the reset of the password' + 
-//                     'Please click on the following link, or paste this into your browser to complete the process ' + 
-//                     'http://' + req.headers.host + '/reset/' + token + '\n\n' + 
-//                     'If you did not request this, please ignore this email and your password will remain unchange'
-
-//             };
-//             smtpTransport.sendMail(mailOptions, function(err) {
-//                 console.log('mail sent');
-//                 req.flash ('success', 'An e-mail has been sent to ' + user.email + 'with further instructions.');
-//                 done(err, 'done');
-//             });
-//         }
-//     ], function(err) {
-//         if (err) return next(err);
-//         res.redirect('/forgot');
-//     });
-// });
-
-// //reset password
-// router.get('/reset/:token', function(req,res) {
-//     User.findOne({ resetPasswordExpires: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-//         if (!user) {
-//             req.flash('error', 'Password reset token is invalid or has expired.');
-//             return res.redirect('/forgot');
-//         }
-//         res.redirect('reset', {token: req.params.token});
-//     });
-// });
-
-// router.post('/reset/:token', function(req,res) {
-//     async.waterfall([
-//         function(done) {  
-//             User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now() } }, function(err, user) {
-//                 if (!user) {
-//                     req.flash('error', 'Password reset token is invalid or has expired.');
-//                     return res.redirect('back');
-//                 }
-//                 if(req.body.password === req.body.confirm) {
-//                     user.setPassword(req.body.password, function(err) {
-//                         user.resetPasswordToken = undefined;
-//                         user.resetPasswordExpires = undefined;
-                        
-//                         user.save(function(err) {
-//                             req.logIn(user, function(err) {
-//                                 done(err,user);
-//                             });
-//                         });
-//                     })
-//                 } else {
-//                     req.flash('error', 'Passwords do not match.');
-//                     return res.redirect('back');
-//                 }
-//             });
-//         },
-//         function(user, done) {
-//            var smtpTransport = nodemailer.createTransport({
-//                service: 'Gmail',
-//                auth: {
-//                    user: 'abdullahhussainbaig1997@gmail.com',
-//                    pass: 'Ahussain@2014'
-//                }
-//            });
-//            var mailOptions = {
-//             to: user.email,
-//             from: 'abdullahhussainbaig1997@gmail.com',
-//             subject: 'Your password has been changed',
-//             text: 'hello,\n\n' +
-//             'This is a confirmation that the password for your account ' + user.email + ' has just changed'
-               
-//            } ;
-//            smtpTransport.sendMail(mailOptions, function(err) {
-//                req.flash('success', 'Success! Your password has been changed.');
-//                done(err);
-//            });
-//         }
-//     ], function(err) {
-//         res.redirect('/home');
-//     });
-// });
+ //middlewere open
+ function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+      return next();
+  }
+  res.redirect("/login");
+}
 
 // forgot password
 router.get('/forgot', function(req, res) {
@@ -254,8 +144,8 @@ router.get('/forgot', function(req, res) {
         });
         var mailOptions = {
           to: user.email,
-          from: 'verify@dreamstore.in',
-          subject: ' Password Reset for Dreamstore',
+          from: 'resetpassword@dreamstore.in',
+          subject: 'Password Reset for Dreamstore',
           text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
             'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
             'http://' + req.headers.host + '/reset/' + token + '\n\n' +
@@ -318,7 +208,7 @@ router.get('/forgot', function(req, res) {
         });
         var mailOptions = {
           to: user.email,
-          from: 'verify@dreamstore.in',
+          from: 'resetpassword@dreamstore.in',
           subject: 'Your password has been changed',
           text: 'Hello,\n\n' +
             'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
@@ -414,13 +304,7 @@ router.get('/forgot', function(req, res) {
   });
 
 
-  //middlewere open
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+ 
 
 
 
