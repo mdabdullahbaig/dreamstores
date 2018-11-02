f6cfb398ac68ef1b9b40f648ce7a8d000e5011ed
Abdullah Baig <abdullahhussainbaig1997@gmail.com>
2018-11-03 02:02:25 +0530

    delete css

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-     'url' : 'mongodb://localhost/STORE',
+     //'url' : 'mongodb://localhost/STORE',
 
-     //'url' : 'mongodb://dreamstore2018:abdul7438@ds131903.mlab.com:31903/dreamstore',
+     'url' : 'mongodb://dreamstore2018:abdul7438@ds131903.mlab.com:31903/dreamstore',
     useNewUrlParser: true
 };
 
